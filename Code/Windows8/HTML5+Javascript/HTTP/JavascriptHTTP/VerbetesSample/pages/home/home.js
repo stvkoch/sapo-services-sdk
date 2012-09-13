9f0d9bf24ef2be0fcef09746865a43c1ee6987b2
Alexandre Rodrigues <alexandrejbr@live.com>
2012-09-13 12:31:59 +0100

    Verbetes Sample improvement
    View state for the credentials supplied in the home page

--- Code/Windows8/HTML5+Javascript/HTTP/JavascriptHTTP/VerbetesSample/pages/home/home.js
+++ Code/Windows8/HTML5+Javascript/HTTP/JavascriptHTTP/VerbetesSample/pages/home/home.js
@@ -1,12 +1,35 @@
-﻿(function () {
+﻿var globalAuthData =
+                    {
+                        username: undefined,
+                        password: undefined,
+                        accessKey: undefined
+                    };
+
+
+(function () {
     "use strict";
 
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+            document.getElementById('username').value = globalAuthData.username || "";
+            document.getElementById('password').value = globalAuthData.password || "";
+            document.getElementById('accessKey').value = globalAuthData.accessKey || "";
         }
     });
 })();
+
+function navigateTo(page) {
+    globalAuthData.username = document.getElementById('username').value;
+    globalAuthData.password = document.getElementById('password').value;
+    globalAuthData.accessKey = document.getElementById('accessKey').value;
+
+    WinJS.Navigation.navigate(page,
+        {
+            username: globalAuthData.username,
+            password: globalAuthData.password,
+            accessKey: globalAuthData.accessKey
+        });
+}
 
