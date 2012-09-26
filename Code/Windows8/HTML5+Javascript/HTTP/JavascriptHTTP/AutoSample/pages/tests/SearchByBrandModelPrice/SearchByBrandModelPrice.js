3c09bb689d718c950d4faba4555e4e52897bfc9c
Alexandre Rodrigues <alexandrejbr@live.com>
2012-09-26 14:25:41 +0100

    Auto SDK Added

---
+++ Code/Windows8/HTML5+Javascript/HTTP/JavascriptHTTP/AutoSample/pages/tests/SearchByBrandModelPrice/SearchByBrandModelPrice.js
@@ -0,0 +1,44 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/SearchByBrandModelPrice/SearchByBrandModelPrice.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
+
+function searchByBrandModelPrice() {
+    //document.getElementById("searchByBrandModelPriceResult").innerHTML = "Search results";
+    var client = new Auto.ServiceClient(GlobalAuth.username, GlobalAuth.password, GlobalAuth.accessKey);
+
+    var params = {};
+
+    params.Brand = document.getElementById("brandParam").value || undefined;
+    params.Model = document.getElementById("modelParam").value || undefined;
+    params.Price = document.getElementById("priceParam").value || undefined;
+
+    params.start = document.getElementById("startParam").value || undefined;
+    params.sort = document.getElementById("sortParam").value || undefined;
+    params.rows = document.getElementById("rowsParam").value || undefined;
+
+    client.asyncSearchByBrandModelPrice(params).then(function (resultText) {
+        var elementById = document.getElementById("searchByBrandModelPriceResult");
+        elementById.appendChild(elementById.ownerDocument.createTextNode(resultText));
+
+    });
+}
---
+++ Code/Windows8/HTML5+Javascript/HTTP/JavascriptHTTP/AutoSample/pages/tests/SearchByTerms/SearchByTerms.js
@@ -0,0 +1,42 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/SearchByTerms/SearchByTerms.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
+
+function searchByTerms() {
+    //document.getElementById("searchByTermsResult").innerHTML = "Search results";
+
+    var client = new Auto.ServiceClient(GlobalAuth.username, GlobalAuth.password, GlobalAuth.accessKey);
+
+    var params = {};
+
+    params.q = document.getElementById("termsParam").value || undefined;
+    params.start = document.getElementById("startParam").value || undefined;
+    params.sort = document.getElementById("sortParam").value || undefined;
+    params.rows = document.getElementById("rowsParam").value || undefined;
+
+    client.asyncSearchByTerms(params).then(function (resultText) {
+        var elementById = document.getElementById("searchByTermsResult");
+        elementById.appendChild(elementById.ownerDocument.createTextNode(resultText));
+        
+    });
+}
