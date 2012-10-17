fbc6baad98b86393b142f8992e8eb8237522863c
Alexandre Rodrigues <alexandrejbr@live.com>
2012-10-17 17:15:27 +0100

    Photos SOAP Sample started

---
+++ Code/Windows8/XAML+CSharp/SOAP/CSharpSOAP_ProxyInAppProj/Photos/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Photos.Pages;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Photos
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            App app = Application.Current as App;
+
+            if (app == null) return;
+
+            this.tbEsbUsername.Text = app.EsbUsername;
+            this.tbEsbPassword.Text = app.EsbPassword;
+            this.tbEsbAccessKey.Text = app.EsbAccessKey;
+        }
+
+        private void UpdateCredentials()
+        {
+            App app = Application.Current as App;
+
+            if (app == null) return;
+
+            app.EsbUsername = this.tbEsbUsername.Text;
+            app.EsbPassword = this.tbEsbPassword.Text;
+            app.EsbAccessKey = this.tbEsbAccessKey.Text;
+        }
+
+        private void BtDummyEchoClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof(DummyEcho));
+        }
+
+        private void BtEditPhotoClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (EditPhotoDetails));
+        }
+
+        private void BtCreatePhotoClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (CreatePhoto));
+        }
+
+        private void BtDeletePhotoClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (DeletePhoto));
+        }
+
+        private void BtGetPhotoDetailsClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (GetPhotoDetails));
+        }
+
+        private void BtCreateAlbumClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (CreateAlbum));
+        }
+
+        private void BtAddPhotoToAlbumClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (AddPhotoToAlbum));
+        }
+
+        private void BtGetUserAlbumsClick(object sender, RoutedEventArgs e)
+        {
+            this.UpdateCredentials();
+            this.Frame.Navigate(typeof (GetUserAlbums));
+        }
+    }
+}
---
+++ Code/Windows8/XAML+CSharp/SOAP/CSharpSOAP_ProxyInAppProj/Photos/SapoServices/Utils/EnsureCredentialsUseContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ServiceModel;
+using Photos.PhotosServiceReference;
+
+namespace Photos.SapoServices.Utils
+{
+    public class EnsureCredentialsUseContext: IDisposable
+    {
+        private const string Esbcredentials = "ESBCredentials";
+        private const string HttpServicesSapoPtDefinitions = "http://services.sapo.pt/definitions";
+        private const string Esbaccesskey = "ESBAccessKey";
+        private const string HttpServicesSapoPtMetadataMarket = "http://services.sapo.pt/Metadata/Market";
+
+        private readonly OperationContextScope _scope;
+
+        public EnsureCredentialsUseContext(
+            string username, string password, string accessKey, PhotosSoapSecureClient client)
+        {
+            EsbCredentials credentials = new EsbCredentials
+            {
+                Username = username,
+                Password = password,
+            };
+
+             _scope = new OperationContextScope(client.InnerChannel);
+                MessageHeader<EsbCredentials> credentialsMessageHeader = 
+                    new MessageHeader<EsbCredentials>(credentials);
+                MessageHeader<string> accessKeyMessageHeader =
+                    new MessageHeader<string>(accessKey);
+
+                OperationContext.Current.OutgoingMessageHeaders
+                    .Add(credentialsMessageHeader
+                    .GetUntypedHeader(Esbcredentials, HttpServicesSapoPtDefinitions));
+
+                OperationContext.Current.OutgoingMessageHeaders
+                    .Add(accessKeyMessageHeader
+                    .GetUntypedHeader(Esbaccesskey, HttpServicesSapoPtMetadataMarket));
+        }
+
+        public void Dispose()
+        {
+            this._scope.Dispose();
+        }
+    }
+}
