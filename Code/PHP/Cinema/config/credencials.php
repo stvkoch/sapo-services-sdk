fcf6c84a7aec90c3fe55756e5a6c6e7ba0f4aad2
Steven Koch <stvkoch@gmail.com>
2013-01-21 17:16:43 +0000

    example php by cinema at sapo service sdk

---
+++ Code/PHP/Cinema/config/credencials.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * ESBAccessKey, ESBUsername and ESBPassword provide by http://sdb.sapo.pt
+ */
+return array( 
+	'ESBAccessKey'=>'DF34E3-1234-43122-1231-123123123',
+	'ESBUsername'=>'sapouserservice@sapo.pt', 
+	'ESBPassword'=>'yourpass',
+	'debug'=>true,
+);
+
---
+++ Code/PHP/Cinema/index.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+* Get credencials data to access service. This is sensive data. Not save in public repository
+*/
+$credencials = require('config/credencials.php');
+
+$soapHeaders = array();
+$soapHeaders[] = new SoapHeader('http://services.sapo.pt/definitions', 'ESBCredentials', array('ESBUsername'=>$credencials['ESBUsername'], 'ESBPassword'=>$credencials['ESBPassword']));
+$soapHeaders[] = new SoapHeader('http://services.sapo.pt/Metadata/Market', 'ESBAccessKey', $credencials['ESBAccessKey'] );
+
+
+/**
+* PHP native SOAP client. http://php.net/manual/en/class.soapclient.php
+*/
+$cinemaClient = new SoapClient("wsdl/cinema.sapo.wsdl", array('trace' => true ));
+
+
+/**
+* @API, https://store.services.sapo.pt/en/Catalog/agenda/agenda-cinema/technical-description
+* Operations related to obtaining points of interest by giving several parameters: geographical coordinates, free text, a location, a zip code, phone number, etc.
+*/
+
+/*
+* @GetMoviesWithShowTimes
+*	- Listar filmes com horários de exibição dentro dos locais e período de tempo. ReleaseCountryId default = PT; LocationIds do ReleaseCountry;  DateTime StartDate default = última quinta-feira; EndDate default = próxima quinta-feira; PageNumber default = 1; RecordsPerPage default = 10, Max itens = 20;
+*/
+$response = $cinemaClient->__soapCall( 
+	'GetMoviesWithShowTimes' , 
+	array( 'PageNumber'=>1, 'RecordsPerPage'=>20 ), 
+	null, $soapHeaders
+);
+
+var_dump($response);
+
+if($credencials['debug']){
+	var_dump($cinemaClient->__getLastRequestHeaders());
+	var_dump($cinemaClient->__getLastResponseHeaders());
+}
+
+/*
+* @GetGenres
+*	- Consultar lista de géneros de filmes. PageNumber defaults to 1; RecordsPerPage defaults to 10;
+*/
+$response = $cinemaClient->__soapCall( 
+	'GetGenres' , 
+	array( 'PageNumber'=>1, 'RecordsPerPage'=>20 ), 
+	null, $soapHeaders
+);
+var_dump($response);
+
+/*
+* @GetContributorRoles
+*	- Listar tipos de contribuições. PageNumber defaults to 1; RecordsPerPage defaults to 1;
+*/
+
+
+/*
+* @GetTheaters
+*	- Lista de salas de cinema na localização, com quantidade de filmes em exibição.
+*/
+
+/*
+* @GetTheatersWithShowTimes
+*	- Listar filmes dentro dos locais com horários de exibição dentro do período de tempo. ReleaseCountryId default = PT; LocationIds do ReleaseCountry; StartDate default = última quinta-feira; EndDate default = próxima quinta-feira; IncludeLocation filtra elemento location default = true; IncludeShowTimeDetails filtra elemento sessions default = true;  PageNumber default = 1; RecordsPerPage default = 10, Max itens 20;
+*/
+
+
+/*
+* @GetPersons
+*	- Listar filmes de acordo com os filtros (paginado). IncludeReleaseWithCountryId default = PT; PageNumber default = 1; RecordsPerPage default = 10, Max itens = 20;
+*
+*
+* @GetRandomMovieQuote
+*	- Retorna uma citação aleatória de uma personagem de um filme
+*
+*
+*
+*
+*
+*/
