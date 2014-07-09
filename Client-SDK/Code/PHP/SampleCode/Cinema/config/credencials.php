586d6a7c60c0ff5195170f9feafeffb8cc33739e
Steven Koch <stvkoch@gmail.com>
2014-07-09 11:49:07 +0100

    igonre credencials file

--- Client-SDK/Code/PHP/SampleCode/Cinema/config/credencials.php
+++ Client-SDK/Code/PHP/SampleCode/Cinema/config/credencials.php
@@ -3,7 +3,7 @@
  * ESBAccessKey, ESBUsername and ESBPassword provide by http://sdb.sapo.pt
  */
 return array( 
-	'ESBAccessKey'=>'DF34E3-1234-43122-1231-123123123',
+	// 'ESBAccessKey'=>'DF34E3-1234-43122-1231-123123123',
 	'ESBUsername'=>'xxx@users.sdb.sapo.pt', 
 	'ESBPassword'=>'xxx',
 	'debug'=>true,
